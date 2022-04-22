7cfc5fc37ea372cac837bf2366343bd53ec8a8d9
Adam-Szitas <example.eu.sk.number@gmail.com>
2022-04-22 14:46:25 +0200

    Weekly update [22.04.2022]

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { HttpService } from '../http/http.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class LoginService {
-  constructor(private httpService: HttpService) {}
+    constructor(private httpService: HttpService) {}
 
-  isValidUserToken(userToken: string | null): boolean {
-    return !!this.httpService.isValidUserToken$(userToken).subscribe({
-      next(response: Object) {
-        return response;
-      },
-      error(error) {
-        console.log(error);
-        //this.errorService.loginLog(error);
-      },
-    });
-  }
+    isValidUserToken(userToken: string | null): boolean {
+        return !!this.httpService.isValidUserToken$(userToken).subscribe({
+            next(response: Object) {
+                return response;
+            },
+            error(error) {
+                console.log(error);
+                //this.errorService.loginLog(error);
+            },
+        });
+    }
 }
--- src/app/ui-components/forms/ui-form/ui-form.component.ts
+++ src/app/ui-components/forms/ui-form/ui-form.component.ts
@@ -2,17 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormInput } from 'src/app/login.interfaces';
 
 @Component({
-  selector: 'app-ui-form',
-  templateUrl: './ui-form.component.html',
-  styleUrls: ['./ui-form.component.scss'],
+    selector: 'app-ui-form',
+    templateUrl: './ui-form.component.html',
+    styleUrls: ['./ui-form.component.scss'],
 })
 export class UiFormComponent implements OnInit {
-  @Input()
-  formGroup!: FormInput;
+    @Input()
+    formGroup!: FormInput;
 
-  constructor() {}
-
-  ngOnInit(): void {
-    console.log(this.formGroup);
-  }
+    ngOnInit(): void {
+        console.log(this.formGroup);
+    }
 }
